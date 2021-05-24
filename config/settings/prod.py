1d86479ff06616be6e857273cbbfbc51c4a7987d
ppjh263 <ppjh8263@naver.com>
2021-05-24 22:02:59 +0900

    settings 복구

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,8 +1,8 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.143.236.157']
+ALLOWED_HOSTS = ['*']
 
-# STATIC_ROOT = BASE_DIR / 'static/'
-# STATICFILES_DIRS = []
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
 
 DEBUG = False
